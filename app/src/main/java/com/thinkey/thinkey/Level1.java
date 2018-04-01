35c44e7dfb0e53a684df8281a894de2332d6edf8
bastianSchuster <bschuster@student.tgm.ac.at>
2018-04-01 18:58:22 +0200

    1st level

--- app/src/main/java/com/thinkey/thinkey/Level1.java
+++ app/src/main/java/com/thinkey/thinkey/Level1.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Level1 extends AppCompatActivity {
 
@@ -18,45 +21,72 @@ public class Level1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
 
-        Button btn= (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity();
-            }
-        });
+
+
+
 
         //ImageButton red = (ImageButton) findViewById(R.id.imageButton);
-        EditText text = (EditText) findViewById(R.id.editText);
-        RadioButton r1= (RadioButton) findViewById(R.id.radioButton);
+        TextView text = (TextView) findViewById(R.id.textView);
+        final  RadioButton r1= (RadioButton) findViewById(R.id.radioButton);
         r1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
-        RadioButton r2= (RadioButton) findViewById(R.id.radioButton2);
+        final RadioButton r2= (RadioButton) findViewById(R.id.radioButton2);
         r2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
-        RadioButton r3= (RadioButton) findViewById(R.id.radioButton3);
+        final RadioButton r3= (RadioButton) findViewById(R.id.radioButton3);
         r3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
-        RadioButton r4= (RadioButton) findViewById(R.id.radioButton4);
+        final RadioButton r4= (RadioButton) findViewById(R.id.radioButton4);
         r4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
+
+        Button btn= (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(r1.isChecked()){
+                    Toast.makeText(getApplicationContext(),"Falsch",Toast.LENGTH_SHORT).show();
+                }
+                if(r2.isChecked()){
+                    Toast.makeText(getApplicationContext(),"Richtig",Toast.LENGTH_SHORT).show();
+                }
+                if(r3.isChecked()){
+                    Toast.makeText(getApplicationContext(),"Falsch",Toast.LENGTH_SHORT).show();
+                }
+                if(r3.isChecked()){
+                    Toast.makeText(getApplicationContext(),"Falsch",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Button back =(Button) findViewById(R.id.button2);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
     }
+
+
+
+
 
     public void openActivity(){
         Intent intent= new Intent(this,MainActivity.class);
