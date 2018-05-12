79d7168f18f4fee00d2468b12a99fecda9a26ba8
bastianSchuster <bschuster@student.tgm.ac.at>
2018-05-12 21:49:57 +0200

    asdf

--- app/src/main/java/com/thinkey/thinkey/Level1_logik.java
+++ app/src/main/java/com/thinkey/thinkey/Level1_logik.java
@@ -51,6 +51,16 @@ public class Level1_logik extends AppCompatActivity {
             }
         });
 
+        View decorView = getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+
     }
 
 }
