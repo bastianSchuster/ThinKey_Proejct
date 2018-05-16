3c0d55a9d9004e6149555df7f0efdabb4515cf26
Daniel Filipovic <dfilipovic@student.tgm.ac.at>
2018-05-16 14:50:50 +0200

    Countdown für das erste Level

--- app/src/main/java/com/thinkey/thinkey/Level1.java
+++ app/src/main/java/com/thinkey/thinkey/Level1.java
@@ -3,6 +3,8 @@ package com.thinkey.thinkey;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +20,9 @@ import org.w3c.dom.Text;
 public class
 Level1 extends AppCompatActivity {
 
-
+    private CountDownTimer countDownTimer;
+    private TextView countDown;
+    private long timeLeftMilliSeconds = 30000; //30 seconds
     private ImageButton btn;
     private Button schwarz,rot;
     TextView tvhighscore;
@@ -28,6 +32,8 @@ Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
+
+        countDown = (TextView) findViewById(R.id.level_1_timer);
 
         schwarz=(Button) findViewById(R.id.Schwarz);
         rot=(Button) findViewById(R.id.Rot);
@@ -62,8 +68,6 @@ Level1 extends AppCompatActivity {
             }
         });
 
-
-
         View decorView = getWindow().getDecorView();
 
         decorView.setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -73,8 +77,40 @@ Level1 extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                 | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
+        startCountDown();
+
     }
 
+    public void startCountDown() {
 
+        countDownTimer = new CountDownTimer(timeLeftMilliSeconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftMilliSeconds = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+    }
+
+    public void updateCountDownText() {
+
+        countDown = (TextView) findViewById(R.id.level_1_timer);
+
+        int seconds = (int) timeLeftMilliSeconds / 1000;
+        countDown.setText(String.valueOf(seconds));
+
+        if (timeLeftMilliSeconds < 10000) {
+            countDown.setTextColor(Color.RED);
+        } else {
+            countDown.setTextColor(Color.WHITE);
+        }
+
+    }
 
 }
